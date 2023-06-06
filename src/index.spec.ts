8208aeb3e9f380c1edcc61167e309aa01c497216
Susisu <susisu2413@yahoo.co.jp>
2023-06-06 19:59:50 +0900

    feat: minimal implementation

---
+++ src/index.spec.ts
@@ -0,0 +1,46 @@
+import type { Component } from ".";
+import { impl, mixer } from ".";
+
+type FooComponent = Component<"foo", { getFoo: () => number }>;
+type BarComponent = Component<"bar", { getBar: () => string }>;
+type BazComponent = Component<"baz", { getBaz: () => boolean }>;
+
+describe("impl", () => {
+  it("creates a factory object that implements a component", () => {
+    const foo = impl<FooComponent, [BarComponent]>("foo", app => ({
+      getFoo: () => app.bar.getBar().length,
+    }));
+    expect(foo.name).toBe("foo");
+    const instance = foo.func({
+      bar: {
+        getBar: () => "Hello",
+      },
+    });
+    expect(instance.getFoo()).toBe(5);
+  });
+});
+
+describe("mixer", () => {
+  const foo = impl<FooComponent, [BarComponent, BazComponent]>("foo", deps => ({
+    getFoo: () => (deps.baz.getBaz() ? deps.bar.getBar().length : 42),
+  }));
+  const bar = impl<BarComponent, [BazComponent]>("bar", deps => ({
+    getBar: () => (deps.baz.getBaz() ? "Hello" : "Bye"),
+  }));
+  const baz = impl<BazComponent, []>("baz", () => ({
+    getBaz: () => true,
+  }));
+
+  it("mixes components and makes a mixed instance", () => {
+    const app = mixer(foo, bar, baz).make();
+    expect(app.foo.getFoo()).toBe(5);
+  });
+
+  it("can override previous mixed components", () => {
+    const baz2 = impl<BazComponent, []>("baz", () => ({
+      getBaz: () => false,
+    }));
+    const app = mixer(foo, bar).mix(baz2).make();
+    expect(app.foo.getFoo()).toBe(42);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,145 @@
-export {};
+const componentKind = Symbol("hokemi.Component");
+
+export type Component<N extends string, T extends unknown> = {
+  kind: typeof componentKind;
+  name: N;
+  type: T;
+};
+
+type AbstractComponent = Component<string, unknown>;
+
+type Instance<C extends AbstractComponent> = C extends Component<infer N, infer T>
+  ? _Instance<N, T>
+  : never;
+type _Instance<N extends string, T extends unknown> = IsFiniteString<N> extends true
+  ? N extends unknown
+    ? { readonly [N0 in N]: T }
+    : never
+  : {};
+// prettier-ignore
+type IsFiniteString<S extends string> =
+    string extends S ? false
+  : S extends "" ? true
+  : S extends `${infer H}${infer R}` ? (
+      string extends H ? false
+    : `${number}` extends H ? false
+    : `${bigint}` extends H ? false
+    : IsFiniteString<R>
+  )
+  : never;
+
+type ComposedInstance<Cs extends AbstractComponent[]> = Cs extends unknown
+  ? {
+      [K in keyof Cs]: (x: Instance<Cs[K]>) => unknown;
+    }[number] extends (x: infer A) => unknown
+    ? A
+    : never
+  : never;
+
+const factoryType = Symbol("hokemi.Factory");
+
+type Factory<N extends string, T extends unknown, D extends unknown> = Readonly<{
+  type: typeof factoryType;
+  name: N;
+  func: (deps: D) => T;
+}>;
+
+type AbstractFactory = Factory<string, unknown, never>;
+
+export type Impl<C extends AbstractComponent, Ds extends AbstractComponent[]> = C extends Component<
+  infer N,
+  infer T
+>
+  ? Factory<N, T, ComposedInstance<Ds>>
+  : never;
+
+type ImplArgs<C extends AbstractComponent, Ds extends AbstractComponent[]> = _ImplArgs<Impl<C, Ds>>;
+type _ImplArgs<F extends AbstractFactory> = F extends Factory<infer N, infer T, infer D>
+  ? [name: N, func: (deps: D) => T]
+  : never;
+
+export function impl<C extends AbstractComponent, Ds extends AbstractComponent[]>(
+  ...[name, func]: ImplArgs<C, Ds>
+): Impl<C, Ds> {
+  const factory: AbstractFactory = {
+    type: factoryType,
+    name,
+    func,
+  };
+  // ImplArgs<C, Ds> and Impl<C, Ds> always have the same shape, so it's safe to cast.
+  // eslint-disable-next-line @susisu/safe-typescript/no-type-assertion
+  return factory as Impl<C, Ds>;
+}
+
+export type Mixer<Fs extends AbstractFactory[]> = Readonly<{
+  mix: MixFunction<Fs>;
+  make: MakeFunction<Fs>;
+}>;
+
+type MixFunction<Fs extends AbstractFactory[]> = <Gs extends AbstractFactory[]>(
+  ...args: Gs
+) => Mixer<[...Fs, ...Gs]>;
+
+type MakeFunction<Fs extends AbstractFactory[]> = MakeError<Fs> extends never
+  ? () => MixedInstance<Fs>
+  : MakeError<Fs>;
+
+type MixedInstance<Fs extends AbstractFactory[]> = ComposedInstance<{
+  [K in keyof Fs]: ReconstructComponent<Fs[K]>;
+}>;
+type ReconstructComponent<F extends AbstractFactory> = F extends Factory<infer N, infer T, never>
+  ? Component<N, T>
+  : never;
+
+type MakeError<Fs extends AbstractFactory[]> = {
+  [K in keyof Fs]: DependencyError<Fs[K], Fs>;
+}[number];
+
+const dependencyError = Symbol("hokemi.error.dependencyError");
+
+type DependencyError<
+  F extends AbstractFactory,
+  Fs extends AbstractFactory[]
+> = MixedInstance<Fs> extends Dependencies<F>
+  ? never
+  : {
+      [dependencyError]: {
+        reason: `missing dependencies`;
+        requiredBy: F["name"];
+        dependencyNames: MissingDependencyNames<Dependencies<F>, MixedInstance<Fs>>;
+      };
+    };
+type Dependencies<F extends AbstractFactory> = F extends Factory<string, unknown, infer D>
+  ? D
+  : never;
+type MissingDependencyNames<D extends unknown, T extends unknown> = D extends unknown
+  ? [Exclude<keyof D, keyof T>]
+  : never;
+
+export function mixer<Fs extends AbstractFactory[]>(...factories: Fs): Mixer<Fs> {
+  const mix: MixFunction<Fs> = (...args) => mixer(...factories, ...args);
+
+  // eslint-disable-next-line @susisu/safe-typescript/no-type-assertion
+  const make: MakeFunction<Fs> = (() => {
+    const app = {};
+    const components: Array<Readonly<{ name: string; value: unknown }>> = [];
+    for (const factory of factories) {
+      components.push({
+        name: factory.name,
+        // eslint-disable-next-line @susisu/safe-typescript/no-type-assertion
+        value: factory.func(app as never),
+      });
+    }
+    for (const component of components) {
+      Object.defineProperty(app, component.name, {
+        value: component.value,
+        configurable: true,
+        enumerable: true,
+        writable: true,
+      });
+    }
+    return app;
+  }) as MakeFunction<Fs>;
+
+  return { mix, make };
+}
