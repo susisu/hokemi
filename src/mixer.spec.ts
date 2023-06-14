fa5febeef68c622a97cc3793244d714a0facccca
Susisu <susisu2413@yahoo.co.jp>
2023-06-15 03:34:28 +0900

    test: add test cases for union types

--- src/mixer.spec.ts
+++ src/mixer.spec.ts
@@ -16,16 +16,19 @@ describe("Mixer", () => {
     type BarComponent = Component<"bar", Bar>;
     type BazComponent = Component<"baz", Baz>;
 
-    type FooImpl = Impl<FooComponent, [BarComponent, BazComponent]>;
-    type BarImpl = Impl<BarComponent, [BazComponent]>;
-    type BazImpl = Impl<BazComponent, []>;
-
     it("creates an instance if there is no error", () => {
+      type FooImpl = Impl<FooComponent, [BarComponent, BazComponent]>;
+      type BarImpl = Impl<BarComponent, [BazComponent]>;
+      type BazImpl = Impl<BazComponent>;
+
       type M = Mixer<[FooImpl, BarImpl, BazImpl]>;
       assertType<Equals<M["new"], () => Mixed<[FooComponent, BarComponent, BazComponent]>>>();
     });
 
-    it("can report missing dependencies errors", () => {
+    it("reports missing dependencies if some dependencies are missing", () => {
+      type FooImpl = Impl<FooComponent, [BarComponent, BazComponent]>;
+      type BarImpl = Impl<BarComponent, [BazComponent]>;
+
       type M = Mixer<[FooImpl, BarImpl]>;
       assertType<
         Equals<
@@ -58,7 +61,11 @@ describe("Mixer", () => {
       >();
     });
 
-    it("can report incompatible dependencies error", () => {
+    it("reports incompatible dependencies if some dependencies are incompatible", () => {
+      type FooImpl = Impl<FooComponent, [BarComponent, BazComponent]>;
+      type BarImpl = Impl<BarComponent, [BazComponent]>;
+      type BazImpl = Impl<BazComponent>;
+
       type Bar2 = { getBar2: () => string };
       type Bar2Component = Component<"bar", Bar2>;
       type Bar2Impl = Impl<Bar2Component, [BazComponent]>;
@@ -82,6 +89,144 @@ describe("Mixer", () => {
           }
         >
       >();
+    });
+
+    it("reports missing dependencies if some dependencies are possibly missing", () => {
+      type FooImpl = Impl<FooComponent, [BarComponent]>;
+      type BarImpl = Impl<BarComponent>;
+      type BarBazImpl = Impl<BarComponent | BazComponent>;
+
+      type M1 = Mixer<[FooImpl, BarBazImpl]>;
+      assertType<
+        Equals<
+          M1["new"],
+          {
+            [missingDependenciesError]: {
+              reason: "provider has missing dependencies";
+              providerName: "foo";
+              dependencies: [
+                {
+                  name: "bar";
+                  expectedType: Bar;
+                }
+              ];
+            };
+          }
+        >
+      >();
+
+      type M2 = Mixer<[FooImpl, BarBazImpl, BarImpl]>;
+      assertType<
+        Equals<M2["new"], () => Mixed<[FooComponent, BarComponent | BazComponent, BarComponent]>>
+      >();
+    });
+
+    it("allows creating an instance if any possible combination of dependencies is provided", () => {
+      type FooImpl = Impl<FooComponent, [BarComponent] | [BazComponent]>;
+      type BarImpl = Impl<BarComponent>;
+      type BazImpl = Impl<BazComponent>;
+
+      type M1 = Mixer<[FooImpl]>;
+      assertType<
+        Equals<
+          M1["new"],
+          {
+            [missingDependenciesError]: {
+              reason: "provider has missing dependencies";
+              providerName: "foo";
+              dependencies:
+                | [
+                    {
+                      name: "bar";
+                      expectedType: Bar;
+                    }
+                  ]
+                | [
+                    {
+                      name: "baz";
+                      expectedType: Baz;
+                    }
+                  ];
+            };
+          }
+        >
+      >();
+
+      type M2 = Mixer<[FooImpl, BarImpl]>;
+      assertType<Equals<M2["new"], () => Mixed<[FooComponent, BarComponent]>>>();
+
+      type M3 = Mixer<[FooImpl, BazImpl]>;
+      assertType<Equals<M3["new"], () => Mixed<[FooComponent, BazComponent]>>>();
+    });
+
+    it("reports missing dependencies unless all possible dependencies are provided", () => {
+      type FooImpl = Impl<FooComponent, [BarComponent]> | Impl<FooComponent, [BazComponent]>;
+      type BarImpl = Impl<BarComponent>;
+      type BazImpl = Impl<BazComponent>;
+
+      type M1 = Mixer<[FooImpl]>;
+      assertType<
+        Equals<
+          M1["new"],
+          {
+            [missingDependenciesError]: {
+              reason: "provider has missing dependencies";
+              providerName: "foo";
+              dependencies: [
+                | {
+                    name: "bar";
+                    expectedType: Bar;
+                  }
+                | {
+                    name: "baz";
+                    expectedType: Baz;
+                  }
+              ];
+            };
+          }
+        >
+      >();
+
+      type M2 = Mixer<[FooImpl, BarImpl]>;
+      assertType<
+        Equals<
+          M2["new"],
+          {
+            [missingDependenciesError]: {
+              reason: "provider has missing dependencies";
+              providerName: "foo";
+              dependencies: [
+                {
+                  name: "baz";
+                  expectedType: Baz;
+                }
+              ];
+            };
+          }
+        >
+      >();
+
+      type M3 = Mixer<[FooImpl, BazImpl]>;
+      assertType<
+        Equals<
+          M3["new"],
+          {
+            [missingDependenciesError]: {
+              reason: "provider has missing dependencies";
+              providerName: "foo";
+              dependencies: [
+                {
+                  name: "bar";
+                  expectedType: Bar;
+                }
+              ];
+            };
+          }
+        >
+      >();
+
+      type M4 = Mixer<[FooImpl, BarImpl, BazImpl]>;
+      assertType<Equals<M4["new"], () => Mixed<[FooComponent, BarComponent, BazComponent]>>>();
     });
   });
 });
