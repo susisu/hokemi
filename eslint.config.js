da4e1b603e54726bb1ceab982bad5a56ebee3afd
Susisu <susisu2413@yahoo.co.jp>
2024-02-28 02:11:42 +0900

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,36 @@
-import { config, map } from "@susisu/eslint-config";
-import prettierConfig from "eslint-config-prettier";
+import { config } from "@susisu/eslint-config";
 import vitestPlugin from "eslint-plugin-vitest";
 import globals from "globals";
 
-export default [
-  ...map({ files: ["src/**/*.ts"] }, [
-    config.tsTypeChecked(),
-    {
-      languageOptions: {
-        sourceType: "module",
-        parserOptions: {
-          project: "./tsconfig.json",
-        },
-        globals: {
-          ...globals.es2021,
-          ...globals.node,
-        },
+export default config({}, [
+  {
+    plugins: {
+      vitest: vitestPlugin,
+    },
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
       },
     },
-  ]),
-  ...map({ files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"] }, [
-    {
-      plugins: {
-        vitest: vitestPlugin,
-      },
-      languageOptions: {
-        globals: {
-          ...vitestPlugin.environments.env.globals,
-        },
-      },
-      rules: {
-        ...vitestPlugin.configs.recommended.rules,
-        "vitest/expect-expect": ["error", { customExpressions: ["expect", "assertType"] }],
+  },
+  {
+    files: ["src/**/*.spec.ts", "src/**/__tests__/**/*.ts"],
+    rules: {
+      ...vitestPlugin.configs.recommended.rules,
+      "vitest/expect-expect": ["error", { assertFunctionNames: ["expect", "assertType"] }],
+    },
+  },
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
       },
     },
-  ]),
-  ...map({ files: ["*.js"] }, [
-    config.js(),
-    {
-      languageOptions: {
-        sourceType: "module",
-        globals: {
-          ...globals.es2021,
-          ...globals.node,
-        },
-      },
-    },
-  ]),
-  prettierConfig,
-];
+  },
+]);
